5cdbaa2f4dbc4dff282aa3a23066e9d2c9fb8fc8
Abhijith-M-Prasad <96982725+Abhijith-M-Prasad@users.noreply.github.com>
2023-11-19 23:11:58 +0530

    added gans

---
+++ gans/config.py
@@ -0,0 +1,19 @@
+# import
+import torch
+
+
+path = "D:\Github\Dataset"
+
+
+# Hyperparameters
+batch_size = 64
+epochs = 1000
+latent_dim = 100
+lrD = 1e-4
+lrG = 1e-4
+optimD = torch.optim.AdamW
+optimG = torch.optim.AdamW
+criterionD = torch.nn.BCELoss()
+criterionG = torch.nn.BCELoss()
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
